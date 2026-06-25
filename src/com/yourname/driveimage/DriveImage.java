45d8820a4bf4e2d1e568dd752788d629565ff123
kumardanish00x-oss <kumardanish00x@gmail.com>
2026-06-25 13:42:51 +0530

    Add DriveImage class to generate image URLs

--- src/com/yourname/driveimage/DriveImage.java
+++ src/com/yourname/driveimage/DriveImage.java
@@ -1,8 +1,28 @@
 package com.yourname.driveimage;
 
-public class DriveImage {
+import com.google.appinventor.components.annotations.DesignerComponent;
+import com.google.appinventor.components.annotations.SimpleFunction;
+import com.google.appinventor.components.annotations.SimpleObject;
+import com.google.appinventor.components.common.ComponentCategory;
+import com.google.appinventor.components.runtime.AndroidNonvisibleComponent;
+import com.google.appinventor.components.runtime.ComponentContainer;
 
-    public static String GetImageUrl(String fileId) {
+@DesignerComponent(
+    version = 1,
+    description = "Google Drive Image Loader",
+    category = ComponentCategory.EXTENSION,
+    nonVisible = true,
+    iconName = "")
+
+@SimpleObject(external = true)
+public class DriveImage extends AndroidNonvisibleComponent {
+
+    public DriveImage(ComponentContainer container) {
+        super(container.$form());
+    }
+
+    @SimpleFunction(description = "Convert Google Drive File ID to Direct URL")
+    public String GetImageUrl(String fileId) {
         return "https://drive.google.com/uc?export=view&id=" + fileId;
     }
 }
